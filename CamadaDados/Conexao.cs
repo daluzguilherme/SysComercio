b7fbab8003bd889d13dfaaa728b517c8fa8b31cd
Guilherme da Luz <guilherme.daluz@outlook.com>
2017-07-11 13:45:07 -0300

    Adicionado relações de camadas

---
+++ CamadaDados/Conexao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CamadaDados
+{
+    class Conexao
+    {
+        public static string Cn = "DataSource = SEDE-VM-003440; Initial Catalog = dbcomercio; Integrated Security = true";
+    }
+}
