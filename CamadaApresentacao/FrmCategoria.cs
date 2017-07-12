5162ac1ed8a447138df99a524123edcdb110603f
Guilherme da Luz <guilherme.daluz@outlook.com>
2017-07-12 12:49:36 -0300

    Campos e botões Form de Categorias

--- CamadaApresentacao/FrmCategoria.cs
+++ CamadaApresentacao/FrmCategoria.cs
@@ -7,14 +7,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CamadaNegocio;
 
 namespace CamadaApresentacao
 {
     public partial class FrmCategoria : Form
     {
+        private bool eNovo = false;
+        private bool eEditar = false;
+
         public FrmCategoria()
         {
             InitializeComponent();
+            this.ttMensagem.SetToolTip(this.txtNome, "Insira o nome da categoria");
+        }
+
+        //Mostrar Mensagem de confirmação
+        private void MensagemOk(string mensagem)
+        {
+            MessageBox.Show(mensagem, "Sistema Comércio", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        //Mostrar Mensagem de erro
+        private void MensagemErro(string mensagem)
+        {
+            MessageBox.Show(mensagem, "Sistema Comércio", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+
+
+        //limpar campos
+        private void Limpar()
+        {
+            this.txtNome.Text = string.Empty;
+            this.txtIdCategoria.Text = string.Empty;
+            this.txtDescricao.Text = string.Empty;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataLista_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmCategoria_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
